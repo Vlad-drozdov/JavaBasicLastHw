40ef36a310d65f976d83b4a0800606b6b250c99d
Vladosik <vladdrozdov7875@gmail.com>
2025-11-15 02:24:56 +0200

    adding

---
+++ src/main/java/org/drozdov/App.java
@@ -0,0 +1,15 @@
+package org.drozdov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println("Add 2 + 4: "+SimpleCalculator.add(2,4));
+        System.out.println("Subtract 12 - 7: "+SimpleCalculator.subtract(12,7));
+        System.out.println("Multiply 5 * 4: "+SimpleCalculator.multiply(5,4));
+        System.out.println("divide 6 / 3: "+SimpleCalculator.divide(6,3));
+    }
+}
